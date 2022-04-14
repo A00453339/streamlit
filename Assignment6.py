e7c142e536291dccfc85c72b553589d91382a0a5
A00453339 <gagandeep.singh.chadha@smu.ca>
2022-04-14 18:27:40 -0300

    used streamlit line chart in place of plotly

--- Assignment6.py
+++ Assignment6.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import requests as rq
-import plotly.express as px
 
 st.header('Bitcoin Prices')
 num_of_days=st.slider('No. of days',1,365,10)
@@ -14,8 +13,8 @@ lol = data.json()['prices']
 df = pd.DataFrame(lol,columns=['date','price'])
 df['date']=pd.to_datetime(df['date'],unit='ms')
 
-fig = px.line(df,x='date',y='price')
-st.plotly_chart(fig)
+df.set_index('date',inplace=True)
+st.line_chart(df)
 
 st.write('Average price during this time was ' + str((df['price'].sum())/num_of_days))
 
